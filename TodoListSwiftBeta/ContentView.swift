adc3d3108e14cd47482afde66b6e6aaf60200c26
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-13 10:50:44 -0600

    Initial Commit

---
+++ TodoListSwiftBeta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoListSwiftBeta
+//
+//  Created by Marco Alonso on 13/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoListSwiftBeta/TodoListSwiftBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftBetaApp.swift
+//  TodoListSwiftBeta
+//
+//  Created by Marco Alonso on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
